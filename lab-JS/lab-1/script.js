1f1001912a7294c365989c62ad151af625060fe0
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-24 14:51:31 +0200

    Refactor index.html and script.js for improved structure; add style.css for button styling

--- lab-JS/lab-1/script.js
+++ lab-JS/lab-1/script.js
@@ -1,9 +1,13 @@
-const divElement = document.getElementById('message-container');
+const headElem = document.getElementsByClassName("heading");
 
-if (divElement) {
-    divElement.textContent = "Hello World!";
+if (headElem.length > 0){
+    headElem[0].textContent = "Hello world! This is changed text!";
 }
 
 function showStudentName() {
     console.error('Sliesarchuk Diana');
+}
+
+function checkMouseIn() {
+    console.log('Mouse in!');
 }
