37c139fdc2a614d06d2c93ad292051e352275449
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-26 18:26:17 +0200

    Add findMinMax for int array function lab_2 task_1.1 JS

--- lab-JS/lab-2/index.js
+++ lab-JS/lab-2/index.js
@@ -1,2 +1,18 @@
-console.log('Happy developing ✨')
+function findLocalMinMax(array) {
+    let localMinMax = new Array(2);
+    localMinMax[0] = array[0];
+    localMinMax[1] = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (localMinMax[0] > array[i]){
+            localMinMax[0] = array[i];
+        }
+        if (localMinMax[1] < array[i]){
+            localMinMax[1] = array[i];
+        }
+    }
+    return localMinMax;
+}
 
+const array = [1, 3, -21, 41, 7, 5, 0, 93];
+console.log('[' + array.join(', ') + ']');
+console.log(findLocalMinMax(array));
