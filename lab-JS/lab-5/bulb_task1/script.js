ca98ad68fb635d1a9c3306f6c992f17e19769568
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-03-22 12:15:53 +0200

    Add bulb flickering/turnoff after afc time runout

--- lab-JS/lab-5/bulb_task1/script.js
+++ lab-JS/lab-5/bulb_task1/script.js
@@ -5,6 +5,8 @@ const body = document.querySelector("body");
 const standardContainer = document.getElementById("standard-container");
 const ledContainer = document.getElementById("led-container");
 const switchBulbBtn = document.getElementById("switch-bulb-btn");
+let inactivityTimeout;
+const TIME_UNTIL_BURNOUT = 6000;
 
 function toggleLight(e) {
     if (e.target.checked) {
@@ -12,6 +14,7 @@ function toggleLight(e) {
     } else {
         body.classList.remove('lighton');
     }
+    resetTimer();
 }
 
 onoffStandard.addEventListener("change", toggleLight);
@@ -21,6 +24,7 @@ switchBulbBtn.addEventListener("click", () => {
     body.classList.remove('lighton');
     onoffStandard.checked = false;
     onoffLED.checked = false;
+    clearTimeout(inactivityTimeout);
 
     if (standardContainer.classList.contains("hidden")) {
         standardContainer.classList.remove("hidden");
@@ -29,4 +33,37 @@ switchBulbBtn.addEventListener("click", () => {
         standardContainer.classList.add("hidden");
         ledContainer.classList.remove("hidden");
     }
+});
+
+function resetTimer() {
+    clearTimeout(inactivityTimeout);
+
+    if (body.classList.contains('lighton') && !body.classList.contains('flicker')) {
+        inactivityTimeout = setTimeout(triggerBurnOut, TIME_UNTIL_BURNOUT);
+    }
+}
+
+function triggerBurnOut() {
+    body.classList.add('flicker');
+
+    const delays = [50, 150, 50, 300, 100, 400];
+    let accumulatedTime = 0;
+
+    delays.forEach((delay) => {
+        accumulatedTime += delay;
+        setTimeout(() => {
+            body.classList.toggle('lighton');
+        }, accumulatedTime);
+    });
+
+    setTimeout(() => {
+        body.classList.remove('lighton');
+        body.classList.remove('flicker');
+        onoffStandard.checked = false;
+        onoffLED.checked = false;
+    }, accumulatedTime + 50);
+}
+
+['mousemove', 'keydown', 'click', 'touchstart'].forEach(evt => {
+    window.addEventListener(evt, resetTimer);
 });
